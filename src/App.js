2d409165a8e0408355da43b88f30798417d62ffa
Md Mehedi Hasan <116740305+engmehedihasan@users.noreply.github.com>
2022-11-22 17:33:30 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Components/Card'
+
+function App(){
+    return <div>
+            <h1 className='head'> React Components </h1>
+            <Card/> <Card/> <Card/> <Card/> <Card/> <Card/>
+            </div>
+}
+
+export default App;
+
---
+++ src/Components/Card.js
@@ -0,0 +1,9 @@
+function Card(){
+    return  <div className='title'>
+            <h3 className='cardTitle'> Mern Stake </h3>
+            <p className='cardDes'> MERN Stack developers have in-depth knowledge of NodeJS, ExpressJS or Restify and have the ability to bind UI elements to JavaScript object models. They have experience creating RESTful services with Node. js and are proficient in OOP and database concepts</p>
+            </div>
+  
+}
+
+export default Card;
